6fc7174496bab72f4bbd81424027321e510aa85c
John Ahn <smileajw1004@gmail.com>
2019-09-12 15:35:06 +0900

    initialize app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
 function App() {
+  const { t } = useTranslation();
+
+  function handleClick(lang) {
+    i18next.changeLanguage(lang)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav style={{ width: '100%', padding: '2rem 0', backgroundColor:'gray' }}>
+          <button onClick={()=>handleClick('en')} >
+            English
+          </button>
+          <button onClick={()=>handleClick('ko')} >
+            Korean
+          </button>
+          <button onClick={()=>handleClick('chi')} >
+            Chinese
+         </button>
+        </nav>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <h3>{t('Thanks.1')}</h3>  <h3>{t('Why.1')}</h3> 
+          </p>
+        </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+import './i18n';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Suspense fallback={(<div>Loading</div>)}>
+        <App />
+    </Suspense>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
